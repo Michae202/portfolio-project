395b69f2035a776e92d001b3713b4401c73b4cf9
Michae202 <111036366+Michae202@users.noreply.github.com>
2023-09-13 21:44:36 +0300

    updated

--- MusicRecommendationSystem/app.py
+++ MusicRecommendationSystem/app.py
@@ -1,19 +1,33 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-
-Once configured, access Spotify content via API...
-client_id = 'YOUR_CLIENT_ID'
-client_secret = 'YOUR_CLIENT_SECRET'
-
-client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-def get_recommendations(seed_artists=[], seed_genres=[], seed_tracks=[]):
-recommendations = sp.recommendations(seed_artists=seed_artists, seed_genres=seed_genres, seed_tracks=seed_tracks)
-tracks = recommendations['tracks']
-for track in tracks:
-print(f"Artist: {track['artists'][0]['name']}, Track: {track['name']}")
-
-# Example usage:
-Seed_artists, seed_genres, and seed_tracks need to be replaced by legitimate facts.
-get_recommendations(seed_artists=['artist_id'], seed_genres=['genre'], seed_tracks=['track_id'])
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+# Load data
+data = pd.read_csv('music_data.csv')
+# Create user-item matrix
+user_item_matrix = data.pivot_table(index='user_id', columns='song_id', values='rating')
+# Compute user similarity
+user_similarity = cosine_similarity(user_item_matrix.fillna(0))
+# Create a function to make recommendations
+def make_recommendations(user_id, num_recommendations):
+    similar_users = user_similarity[user_id-1].argsort()[::-1]
+    similar_users = similar_users[1:num_recommendations+1]
+    similar_user_ids = [x+1 for x in similar_users]
+    recommendations = pd.DataFrame()
+    for user in similar_user_ids:
+        top_songs = user_item_matrix.loc[user].sort_values(ascending=False).head(num_recommendations).index
+        recommendations = pd.concat([recommendations, pd.DataFrame(top_songs)], axis=1)
+    recommendations = recommendations.melt()['song_id'].dropna().drop_duplicates().head(num_recommendations)
+    return recommendations.tolist()
+# Evaluate the system
+y_true = data['song_id']
+y_pred = data['user_id'].apply(lambda x: make_recommendations(x, 10)[0])
+accuracy = accuracy_score(y_true, y_pred)
+print('Accuracy:', accuracy)
+# Add a comment
+# This code is for a music recommender system on Spotify. 
+# It takes in a user ID and returns a list of recommended songs. 
+# The recommendations are based on the user's listening history and the listening history of other users who are similar to the user.
+ 
